7ad1e0635dbeaecb3e520da54406046e01a46a44
CryptoMorki3s <cryptomorki3s@gmail.com>
2024-09-23 19:38:43 +0100

    minor changes

--- backend/lib/utils/generateToken.js
+++ backend/lib/utils/generateToken.js
@@ -9,6 +9,6 @@ module.exports.generateTokenAndSetCookie = (userId, res) => {
 		maxAge: 15 * 24 * 60 * 60 * 1000, // MS
 		httpOnly: true, // prevent XSS attacks cross-site scripting attacks
 		sameSite: 'lax', // Allow cookies to be sent in cross-origin requests
-		secure: process.env.NODE_ENV !== "development",
+		secure: true,
 	});
 };
